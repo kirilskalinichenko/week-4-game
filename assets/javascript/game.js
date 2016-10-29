4bfcb282cc02bafab07d6c80d97cdaa321f2b8f4
kirilskalinichenko <kirilkalinichenko@KKMacBook-Air.local>
2016-10-29 01:05:03 -0500

    some updates

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+var wins = 0;
+var loss = 0;
+var score = 0;
+var runningtotal = 0;
+var random 
+
+
+
+$(document).ready(function() {
+
+		$('.random').Math.floor((Math.random() * 10) + 1);
+
+		$('img').on("click", function () {
+			// body...
+			var random = Math.floor(Math.random() + 1;
+		})
+
+});
